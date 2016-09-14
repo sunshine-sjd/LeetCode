1a30d8e09fd427407717ed34d3cb9113208f73e5
sunshine-sjd <xiaosarenzhe@163.com>
2016-09-14 11:07:02 +0800

    Update reverse-vowels-of-a-string.py

--- reverse-vowels-of-a-string.py
+++ reverse-vowels-of-a-string.py
@@ -4,12 +4,12 @@ def reverseVowels(s):
     start = 0                   # 列表开头
     end = len(s)-1              # 列表末尾
 
-    while start < end:          #
-        if list_s[start] not in total_vowels:
+    while start < end:          
+        if list_s[start] not in total_vowels:    # 从列表首到尾，判断是否是元音字母，不是的index加1
             start += 1
-        elif list_s[end] not in total_vowels:
-            end -= 1
-        else:
+        elif list_s[end] not in total_vowels:    # 从列表尾到首，判断是否是元音字母，不是的index减一
+            end -= 1 
+        else:                                    # 当两边都是元音字母时交换             
             list_s[start], list_s[end] = list_s[end], list_s[start]
             start += 1
             end -= 1
